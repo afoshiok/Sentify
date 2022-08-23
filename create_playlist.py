c620d70f4d040194de9967674320a7dcd98f041b
afoshiok <89757138+afoshiok@users.noreply.github.com>
2022-08-23 10:21:28 -0500

    A bit of linting

--- create_playlist.py
+++ create_playlist.py
@@ -1,14 +1,24 @@
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 import spotipy #pip install spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
 load_dotenv() #Loads enviornment variables
 
 spotify_client = os.environ["spotify_client_id"]
 spotify_token = os.environ["spotify_token"]
 
-spot = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=spotify_client, client_secret=spotify_token))
+#Allows user to login, Spotify login opens on port 8080
+SCOPE = 'playlist-read-private'
+spot = spotipy.Spotify(auth_manager=SpotifyOAuth(
+    client_id=spotify_client,
+    client_secret=spotify_token,
+    redirect_uri='http://localhost:8080',
+    scope=SCOPE)
+    )
 
-results = spot.search(q='weezer', limit=20)
-for idx, track in enumerate(results['tracks']['items']):
-    print(idx, track['name'])
+
+
+results = spot.current_user_playlists(limit=50)
+
+for i, item in enumerate(results['items']):
+    print("%d %s" % (i, item['name']))
