f5e4c804ba2c1f2808509bc53e9eee08960f4d43
afoshiok <89757138+afoshiok@users.noreply.github.com>
2022-08-30 23:15:22 -0500

    Created recommendation function, not yet tested

--- create_playlist.py
+++ create_playlist.py
@@ -1,20 +1,23 @@
 import os
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
 import pprint
 load_dotenv()
 
 spot_client = os.environ["spotify_client_id"]
 spot_token = os.environ["spotify_token"]
-auth_manager = SpotifyClientCredentials(
+scopes = [] #Scopes I need to get this app to work. List of scopes here: https://developer.spotify.com/documentation/general/guides/authorization/scopes/ 
+auth_manager = SpotifyOAuth(
     client_id=spot_client,
     client_secret=spot_token,
-    requests_session=True
+    redirect_uri="http://localhost:8080"
     )
 spot = spotipy.Spotify(auth_manager=auth_manager)
 
-search_inp = input("What artist are you looking for?")
-result = spot.search(search_inp)
-pp = pprint.PrettyPrinter(indent=3)
-pp.pprint(result)
+def get_recs():
+    recommendations = spot.recommendations(
+        seed_artists=['https://open.spotify.com/artist/7gW0r5CkdEUMm42w9XpyZO?si=8N9RAXfZTW6zxvFSqfyrPw'], # To seed multiple artist create a list of their URLs, IDs or URIs (Maximum of 5 artists)
+        seed_tracks= None, #I want this app to seed the users previously listened to tracks. I need a "user-read-recently-played" scope
+        limit= 15 #The api can generate a maximum of a 100 songs, I haven't settled yet on how many songs I want in the playlist.
+        )
