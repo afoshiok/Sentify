341607b99e42a610cbf64301648e34c4277b3ba0
afoshiok <89757138+afoshiok@users.noreply.github.com>
2022-09-02 20:35:43 -0500

    Update api.py

--- backend/api.py
+++ backend/api.py
@@ -1,1 +1,8 @@
 from fastapi import FastAPI
+from create_playlist import create_playlist,find_playlist,get_seeds,get_recs
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
