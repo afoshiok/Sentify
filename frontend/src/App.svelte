490c2b9a78a56c83b8025b772f090a221a4a42fd
afoshiok <89757138+afoshiok@users.noreply.github.com>
2022-10-27 16:18:22 -0500

    Svelte + Tailwindcss Working

---
+++ frontend/src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import svelteLogo from './assets/svelte.svg'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+  <h1 class= "text-3xl">Hello</h1>
+</main>
+
+<style global lang="postcss">
+  @tailwind utilities;
+  @tailwind components;
+  @tailwind base;
+</style>
---
+++ frontend/tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html",'./src/**/*.{svelte,js,ts}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  variants: {
+    extend: {},
+  },
+  darkMode: false, // or 'media' or 'class'
+}
+
