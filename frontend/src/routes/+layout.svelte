9e0b87a3ebd67ac342bb25d01f37eca6bb6c5d22
afoshiok <89757138+afoshiok@users.noreply.github.com>
2022-10-26 20:56:47 -0500

    Playing around with Tailwind + SvelteKit

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,5 +1,10 @@
 <script>
   import "../app.css";
 </script>
+<div class="navbar bg-rose-200 text-primary-content">
+  <a class="btn btn-ghost normal-case text-2xl">daisyUI</a>
+</div>
 
-<slot />
+<body class="bg-rose-200">
+  <slot />
+</body>
