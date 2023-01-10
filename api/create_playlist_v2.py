897698cdd7e4f0698a599a4966b44bb1f8642025
Favour Oshiokhale <89757138+afoshiok@users.noreply.github.com>
2023-01-10 17:07:07 -0600

    Changed noobie mistakes for top artists

--- api/create_playlist_v2.py
+++ api/create_playlist_v2.py
@@ -1,1 +1,45 @@
-#IM GOING TO BE REDOING THE CREATE_PLAYLIST.PY AND MAKING IT BETTER
+import os
+import spotipy #pip install spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from dotenv import load_dotenv #pip install python-dotenv 
+import datetime
+import json
+load_dotenv()
+
+def login():
+    spot_client = os.environ["spotify_client_id"]
+    spot_token = os.environ["spotify_token"]
+    redirect = os.environ["redirect_uri"]
+    scopes = [
+        "user-top-read",
+        "playlist-read-private",
+        "user-top-read"
+    ]
+    auth_manager = SpotifyOAuth(
+        client_id= spot_client,
+        client_secret=spot_token,
+        redirect_uri= redirect,
+        scope=scopes
+    )
+    
+    global spot
+    spot = spotipy.Spotify(auth_manager=auth_manager)
+    user = spot.me()['id']
+
+    return print("Current user: {}".format(user))
+
+def tops(type,term: str):
+    if type == "artist":
+        top_artists = spot.current_user_top_artists(limit=5,time_range=term)
+        artist_dict = {} #Holds all data need for the frontend as well as artist URI for song recommendations
+        for artist in top_artists['items']:
+            artist_dict[artist['name']] = [artist['images'][0]['url'], artist['uri']]
+        print(artist_dict)
+    elif type == "track":
+        top_tracks = spot.current_user_top_tracks(limit=5,time_range=term)
+        return top_tracks
+
+
+if __name__ == "__main__":
+    login()
+    tops("artist","medium_term")
