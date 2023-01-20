f5a26391dc377e113ba1ce254150c187537991c6
Favour Oshiokhale <89757138+afoshiok@users.noreply.github.com>
2023-01-20 17:33:18 -0600

    Update Hello.vue

--- frontend/sentify/src/components/Hello.vue
+++ frontend/sentify/src/components/Hello.vue
@@ -1,5 +1,7 @@
 <template>
-    <section class="flex justify-center align-center min-h-screen">
-        <button>Login</button>
+    <section class="flex justify-center items-center align-center min-h-screen">
+        <button class="bg-white rounded-lg">
+            <span>Login</span>
+        </button>
     </section>
 </template>
