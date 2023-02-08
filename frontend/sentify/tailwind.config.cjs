fccdf69be320993cd84183b1d4f3ca5cb52d406c
Favour Oshiokhale <89757138+afoshiok@users.noreply.github.com>
2023-02-08 14:41:36 -0600

    Update tailwind.config.cjs

--- frontend/sentify/tailwind.config.cjs
+++ frontend/sentify/tailwind.config.cjs
@@ -7,7 +7,7 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui"), require("tailwind-scrollbar")],
+  plugins: [require("daisyui"), require("tailwind-scrollbar")({ nocompatible: true })],
 
   daisyui: {
     styled: true,
