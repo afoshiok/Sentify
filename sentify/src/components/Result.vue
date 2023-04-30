4820e4fd0f27520948966843afcebe095d68dd21
Favour Oshiokhale <89757138+afoshiok@users.noreply.github.com>
2023-04-30 00:12:38 -0500

    Minor changes

--- sentify/src/components/Result.vue
+++ sentify/src/components/Result.vue
@@ -1,6 +1,6 @@
 <template>
     <h1> {{ resultObj }} </h1>
-    <button class="mt-4"></button>
+    <button class="mt-4 bg-white rounded-lg h-14 w-72 p-2 flex flex-row justify-center items-center outline-4 outline-dashed"> Show me the playlist! </button>
 </template>
 
 <script setup lang="ts">
@@ -8,8 +8,11 @@ import { ref, onBeforeMount } from 'vue';
 import { useResultStore } from '../stores/loginStore';
 const resultObj = ref()
 const resultStore = useResultStore()
+const playlistLink = ref('')
+
 
 onBeforeMount(() => {
-    resultObj.value = resultStore
+    resultObj.value = resultStore.result
+    playlistLink.value = resultStore.result.links
 })
 </script>
